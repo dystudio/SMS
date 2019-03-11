65186e9238ef1638251c3e2ddc8daabadec33407
HOHO <591310381@qq.com>
2019-03-11 18:05:54 +0800

    update

--- src/Sms.WebAdmin/Scripts/imgupload.js
+++ src/Sms.WebAdmin/Scripts/imgupload.js
@@ -1,42 +1,42 @@
-﻿//默认为单图上传
-var uploadType = 0;
-//触发上传的按钮对象
-var uploadObj = null;
+﻿////默认为单图上传
+//var uploadType = 0;
+////触发上传的按钮对象
+//var uploadObj = null;
 
-$(function () {
+//$(function () {
 
-    $("#imgUpload").ajaxForm(function (obj) {
-        if (obj.Status) {
-            var $athis = $(uploadObj);
-            var $_moreImg = $athis.next(".thumbnails");
-            var $_input = $athis.prevAll("input[type='hidden']");
-            var newImg = '<li class="span2"> <a href="javascript:" title="点击放大"  class="thumbnail"> <img src="/Upload/Image/' + obj.Url + '" alt=""> </a> <div class="actions"> <a title="移除" href="javascript:" class="op"><i class="icon-remove icon-white"></i></a> </div> </li>';
-            if (uploadType == 1) {//多图列表
-                $_moreImg.append(newImg);
-                var oldValue = $_input.val();//拿到旧值
-                if (oldValue && oldValue != "") {
-                    oldValue += "$" + obj.Url;
-                }
-                else {
-                    oldValue += obj.Url;
-                }
-                //设值
-                $_input.val(oldValue);
-            }
-            else if (uploadType == 0) {//单图
-                $_moreImg.html(newImg);
-                $_input.val(obj.Url);
-            }
-            BindEvent();
-        }
-        else {
-            show_message(false, obj.MsgText, null);
+//    $("#imgUpload").ajaxForm(function (obj) {
+//        if (obj.Status) {
+//            var $athis = $(uploadObj);
+//            var $_moreImg = $athis.next(".thumbnails");
+//            var $_input = $athis.prevAll("input[type='hidden']");
+//            var newImg = '<li class="span2"> <a href="javascript:" title="点击放大"  class="thumbnail"> <img src="/Upload/Image/' + obj.Url + '" alt=""> </a> <div class="actions"> <a title="移除" href="javascript:" class="op"><i class="icon-remove icon-white"></i></a> </div> </li>';
+//            if (uploadType == 1) {//多图列表
+//                $_moreImg.append(newImg);
+//                var oldValue = $_input.val();//拿到旧值
+//                if (oldValue && oldValue != "") {
+//                    oldValue += "$" + obj.Url;
+//                }
+//                else {
+//                    oldValue += obj.Url;
+//                }
+//                //设值
+//                $_input.val(oldValue);
+//            }
+//            else if (uploadType == 0) {//单图
+//                $_moreImg.html(newImg);
+//                $_input.val(obj.Url);
+//            }
+//            BindEvent();
+//        }
+//        else {
+//            show_message(false, obj.MsgText, null);
 
-        }
-    });
-    //初始化事件绑定
-    BindEvent();
-});
+//        }
+//    });
+//    //初始化事件绑定
+//    BindEvent();
+//});
 
 //绑定通用事件
 function BindEvent() {
@@ -60,3 +60,50 @@ function BindEvent() {
 }
 
 
+var defaultOpts = {
+    mode: 1,
+    maxSize: 2048000,
+    ext: 'jpg,jpeg,bmp,png'
+};
+
+$.fn.extend({
+    uploadImage: function (option) {
+        var _this = $(this)[0];
+        console.log($(this))
+        option = $.extend(option, defaultOpts);
+        var formid = "upload_from_" + _this.id, btnid = "upload_btn_" + _this.id;
+        document.write("<form id=\"" + formid + "\" action=\"/FileHandler/Image\" enctype=\"multipart/form-data\" method=\"post\" style=\"display:none\"> <input type=\"file\" id=\"" + _this.id + "\" name=\"filedata\" /> </form>");
+        _this.onclick = function () { $("#" + btnid).click(); }
+        $("#" + formid).ajaxForm(function (obj) {
+            if (obj.Status) {
+                var $athis = $(_this);
+                var $_moreImg = $athis.next(".thumbnails");
+                var $_input = $athis.prevAll("input[type='hidden']");
+                var newImg = '<li class="span2"> <a href="javascript:" title="点击放大"  class="thumbnail"> <img src="' + obj.Url + '" alt=""> </a> <div class="actions"> <a title="移除" href="javascript:" class="op"><i class="icon-remove icon-white"></i></a> </div> </li>';
+                if (option.mode == 1) {//多图列表
+                    $_moreImg.append(newImg);
+                    var oldValue = $_input.val();//拿到旧值
+                    if (oldValue && oldValue != "") {
+                        oldValue += "$" + obj.Url;
+                    }
+                    else {
+                        oldValue += obj.Url;
+                    }
+                    //设值
+                    $_input.val(oldValue);
+                }
+                else if (option.mode == 0) {//单图
+                    $_moreImg.html(newImg);
+                    $_input.val(obj.Url);
+                }
+                BindEvent();
+            }
+            else {
+                show_message(false, obj.MsgText, null);
+            }
+        });
+        $("#" + btnid).bind("change", function () {
+            $("#" + formid).submit();
+        });
+    }
+});
