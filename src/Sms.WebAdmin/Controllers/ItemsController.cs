c6882f8abae012dd6b1031f439da6086e8c291c4
HOHO <591310381@qq.com>
2019-03-26 17:29:20 +0800

    update

---
+++ src/Sms.WebAdmin/Controllers/ItemsController.cs
@@ -0,0 +1,35 @@
+﻿using Sms.Common;
+using Sms.WebAdmin.Common;
+using Sms.WebAdmin.Filter;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Webdiyer.WebControls.Mvc;
+
+namespace Sms.WebAdmin.Controllers
+{
+    public class ItemsController : BaseController
+    {
+        [PermissionFilterAttribute(false, EnumHepler.ActionPermission.View)]
+        public ActionResult Index(int? level, string keyword = "")
+        {
+            var list = _repositoryFactory.IItemInfo.Where(c => c.IsDelete == false);
+            //搜索关键字过滤
+            if (level.HasValue)
+            {
+                list = list.Where(c => c.BrandId == level.Value);
+            }
+            if (!string.IsNullOrEmpty(keyword))
+            {
+                list = list.Where(c => c.Title.Contains(keyword) || c.ItemCode.Equals(keyword));
+            }
+            var pagerList = list.Include(x => x.ItemBrand).OrderByDescending(c => c.CreateTime).ToPagedList(PageIndex, ConstFiled.PageSize);
+            if (Request.IsAjaxRequest())
+                return PartialView("_PartialItemList", pagerList);
+            return View(pagerList);
+        }
+    }
+}
---
+++ src/Sms.WebAdmin/Controllers/OrderController.cs
@@ -0,0 +1,35 @@
+﻿using Sms.Common;
+using Sms.WebAdmin.Common;
+using Sms.WebAdmin.Filter;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Webdiyer.WebControls.Mvc;
+
+namespace Sms.WebAdmin.Controllers
+{
+    public class OrderController : BaseController
+    {
+        [PermissionFilterAttribute(false, EnumHepler.ActionPermission.View)]
+        public ActionResult Index(int? level, string keyword = "")
+        {
+            var list = _repositoryFactory.IOrders.Where(c =>true);
+            //搜索关键字过滤
+            //if (level.HasValue)
+            //{
+            //    list = list.Where(c => c.BrandId == level.Value);
+            //}
+            if (!string.IsNullOrEmpty(keyword))
+            {
+                list = list.Where(c => c.OrderCode.Equals(keyword) || c.OpenId.Equals(keyword));
+            }
+            var pagerList = list.OrderByDescending(c => c.CreateTime).ToPagedList(PageIndex, ConstFiled.PageSize);
+            if (Request.IsAjaxRequest())
+                return PartialView("_PartialOrderList", pagerList);
+            return View(pagerList);
+        }
+    }
+}
